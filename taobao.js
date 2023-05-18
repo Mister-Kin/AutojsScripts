99a31a07573a5623ee3d91cf707ea4f22deb1bc4
Mr. Kin <im.misterkin@gmail.com>
2023-05-19 00:40:37 +0800

    fix: getDailyGoldCoin not work

--- taobao.js
+++ taobao.js
@@ -24,7 +24,7 @@ function getDailyGoldCoin() {
         let detect_get_gold_coin_page_state = common.detectWidgetItem("textContains", "已连签", "none", "normal");
         if (detect_get_gold_coin_page_state) {
             console.log("已进入领淘金币页面");
-            let detect_sign_button = common.detectWidgetItem("textContains", "赚金币", "error", "normal");
+            let detect_sign_button = common.detectWidgetItem("textContains", "领今日金币", "error", "normal");
             if (detect_sign_button) {
                 detect_sign_button.click();
                 // click(540, 660);
