7bb1cacd48385aafab46b7f447b3a12edf69d3b1
Mr. Kin <im.misterkin@gmail.com>
2024-04-01 04:38:45 +0800

    fix: not work for realme neo 3

--- clear-all-recent-apps.js
+++ clear-all-recent-apps.js
@@ -1,17 +1,28 @@
+// ----------------------------
+// 脚本信息设定
 let task_name = "清理最近应用";
+// ----------------------------
 let common = require('common.js');
 common.startLog(task_name);
+// ----------------------------
+// 脚本自定义功能
 recents();
-sleep(5000);
-let detect_clear_all_button = id("com.android.systemui:id/recent_igmbutton_clear_all").findOnce();
-if (detect_clear_all_button) {
-    detect_clear_all_button.click();
-    sleep(5000);
+let detect_clear_all_button1 = common.detectWidgetItem("id", "com.android.launcher:id/btn_clear", "none", "normal");
+if (detect_clear_all_button1) {
+    detect_clear_all_button1.click();
     console.log("已清理「最近应用」");
 }
 else {
-    console.log("未检测到「清理」按钮");
+    let detect_clear_all_button2 = common.detectWidgetItem("id", "com.android.systemui:id/recent_igmbutton_clear_all", "none", "normal");
+    if (detect_clear_all_button2) {
+        detect_clear_all_button2.click();
+        console.log("已清理「最近应用」");
+    }
+    else {
+        console.log("未检测到「清理」按钮");
+    }
 }
+// ----------------------------
 common.endLog(task_name);
 home();
 exit();
