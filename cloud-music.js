d99d7b149b697e51343675878c28c3eeb0f87763
Mr. Kin <im.misterkin@gmail.com>
2024-05-28 01:00:51 +0800

    feat: daily_sign

--- cloud-music.js
+++ cloud-music.js
@@ -15,73 +15,28 @@ common.endLog(task_name);
 home();
 exit();
 
-function getDailyPoints() {
+function daily_sign() {
     let detect_drawer_menu_button = common.detectWidgetItem("desc", "抽屉菜单", "error", "normal");
     if (detect_drawer_menu_button) {
         detect_drawer_menu_button.click();
         let detect_points_center_button = common.detectWidgetItem("textContains", "云贝中心", "error", "normal");
         if (detect_points_center_button) {
             detect_points_center_button.parent().parent().click();
-            if (common.detectSuccessInfo("textContains", "云贝账单")) {
+            if (common.detectSuccessInfo("textContains", "每日签到")) {
                 console.log("已进入云贝中心");
-                console.log("已领取「签到的云贝」");
-                // 目前的版本中，text 的末尾含有空格，为避免这种状况，将 text 选择器换为 textContains 选择器
-                let detect_points_from_listening_music = textContains("听音乐30分钟").find();
-                let detect_points_from_listening_podcast = textContains("听播客30秒").find();
-                let detect_points_from_creating_song_list = textContains("创建一个歌单").find();
-                let try_time = 0;
-                while (detect_points_from_listening_music.empty()) {
-                    sleep(100);
-                    detect_points_from_listening_music = textContains("听音乐30分钟").find();
-                    try_time++;
-                    if (try_time > 10) {
-                        common.detectWidgetItemLog("log", "听音乐30分钟", 10);
-                        break;
-                    }
+                let detect_already_sign_button = common.detectWidgetItem("text", "已签到", "error", "normal");
+                if (detect_already_sign_button) {
+                    console.info("今日已签到，无需重复签到");
                 }
-                try_time = 0;
-                while (detect_points_from_listening_podcast.empty()) {
-                    sleep(100);
-                    detect_points_from_listening_podcast = textContains("听播客30秒").find();
-                    try_time++;
-                    if (try_time > 10) {
-                        common.detectWidgetItemLog("log", "听播客30秒", 10);
-                        break;
+                else {
+                    let detect_sign_button = common.detectWidgetItem("text", "签到", "error", "normal");
+                    if (detect_sign_button) {
+                        detect_sign_button.parent().click();
+                        console.log("签到成功");
                     }
-                }
-                try_time = 0;
-                while (detect_points_from_creating_song_list.empty()) {
-                    sleep(100);
-                    detect_points_from_creating_song_list = textContains("创建一个歌单").find();
-                    try_time++;
-                    if (try_time > 10) {
-                        common.detectWidgetItemLog("log", "创建一个歌单", 10);
-                        break;
+                    else {
+                        console.error("未能找到签到按钮");
                     }
-                }
-                if (detect_points_from_listening_music.nonEmpty()) {
-                    detect_points_from_listening_music.forEach(function (element) {
-                        recursiveClick(element);
-                        sleep(1000);
-                    }
-                    );
-                    console.log("已领取「听音乐的云贝」");
-                }
-                if (detect_points_from_listening_podcast.nonEmpty()) {
-                    detect_points_from_listening_podcast.forEach(function (element) {
-                        recursiveClick(element);
-                        sleep(1000);
-                    }
-                    );
-                    console.log("已领取「听播客的云贝」");
-                }
-                if (detect_points_from_creating_song_list.nonEmpty()) {
-                    detect_points_from_creating_song_list.forEach(function (element) {
-                        recursiveClick(element);
-                        sleep(1000);
-                    }
-                    );
-                    console.log("已领取「创建一个歌单的云贝」");
                 }
             }
             else {
@@ -89,10 +44,4 @@ function getDailyPoints() {
             }
         }
     }
-}
-
-function recursiveClick(element) {
-    if (element != null)
-        if (!element.click())
-            recursiveClick(element.parent());
 }
