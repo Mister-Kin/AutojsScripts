e14cbb96f8c4913483dd2ade13cef723d03377c6
Mr. Kin <im.misterkin@gmail.com>
2026-07-19 03:46:04 +0800

    fix: dailySign not working

--- china-unicom.js
+++ china-unicom.js
@@ -16,22 +16,30 @@ home()
 exit()
 
 function dailySign() {
-    let detect_my_button = common.detectWidgetItem("id", "com.sinovatech.unicom.ui:id/main_tab_user_layout", "error", "normal")
+    // 以下注释方案已失效：因为「我的」页面的签到入口，已经改成了跳转其他页面
+    // let detect_my_button = common.detectWidgetItem("id", "com.sinovatech.unicom.ui:id/main_tab_user_layout", "error", "normal")
+    // if (detect_my_button) {
+    //     detect_my_button.click()
+    //     let detect_in_my_page = common.detectWidgetItem("text", "我的套餐", "error", "normal")
+    //     if (detect_in_my_page) {
+    //         console.log("已进入「我的」页面")
+    //         let detect_sign_button = common.detectWidgetItem("id", "com.sinovatech.unicom.ui:id/user_login_qiandao", "error", "normal")
+    //         if(detect_sign_button){
+    //             detect_sign_button.click()
+    //             if (common.detectSuccessInfo("textContains", "话费红包")) {
+    //                 console.log("已完成「每日签到」")
+    //             }
+    //         }
+    //     }
+    // }
+    let detect_my_button = common.detectWidgetItem("desc", "签到", "error", "normal")
     if (detect_my_button) {
         detect_my_button.click()
-        let detect_in_my_page = common.detectWidgetItem("text", "我的套餐", "error", "normal")
-        if (detect_in_my_page) {
-            console.log("已进入「我的」页面")
-            let detect_sign_button = common.detectWidgetItem("id", "com.sinovatech.unicom.ui:id/user_login_qiandao", "error", "normal")
-            if(detect_sign_button){
-                detect_sign_button.click()
-                if (common.detectSuccessInfo("textContains", "话费红包")) {
-                    console.log("已完成「每日签到」")
-                }
-            }
+        if (common.detectSuccessInfo("textContains", "连续签到")) {
+            console.log("已完成「每日签到」")
         }
     }
     else {
-        console.error("未检测到「我的」按钮")
+        console.error("未检测到「签到」按钮")
     }
 }
